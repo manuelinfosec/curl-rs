1c34fdf683df547a06e825feaeac9f9dc2669994
Manuel <manuelinfosec@gmail.com>
2024-03-31 14:22:00 -0700

    parsing command-line arguments

---
+++ src/cli.rs
@@ -0,0 +1,53 @@
+use clap::{Arg, ArgMatches, Command};
+
+/// Retrieves and parses command-line arguments using Clap for a custom Curl application in Rust.
+///
+/// # Returns
+///
+/// `ArgMatches`: A structure containing parsed command-line arguments.
+///
+/// # Example
+///
+/// ```rust
+/// use clap::ArgMatches;
+///
+/// let args = get_arguments();
+/// let url = args.value_of("url").unwrap_or("");
+/// let method = args.value_of("x-method").unwrap_or("GET");
+/// let data = args.value_of("data").unwrap_or("");
+/// let verbose = args.is_present("verbose");
+/// ```
+pub fn get_arguments() -> ArgMatches {
+    // Initialize a command-line program
+    Command::new("curl-rs")
+        // Program description
+        .about("Transfer data over the HTTP protocol")
+        // Program version
+        .version("1.0")
+        // Program author
+        .author("Manuel <manuelinfosec@gmail.com>")
+        // First mandatory argument
+        .arg(Arg::new("url").index(1).required(true))
+        // Other arguments
+        .arg(
+            Arg::new("x-method")
+                .short('X')
+                .long("X-Method")
+                .help("HTTP method"),
+        )
+        .arg(Arg::new("data").short('d').long("data").help("Payload"))
+        .arg(
+            Arg::new("headers")
+                .short('H')
+                .long("header")
+                .action(clap::ArgAction::Append),
+        )
+        .arg(
+            Arg::new("verbose")
+                .short('v')
+                .long("ver bose")
+                .action(clap::ArgAction::SetTrue)
+                .help("Verbose mode"),
+        )
+        .get_matches()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::ArgMatches;
+
+mod cli;
+mod http;
+
 fn main() {
-    println!("Hello, world!");
+    // parse command line arguments
+    let matches: ArgMatches = cli::get_arguments();
+
+    // check if verbose exists and collect its value
+    let verbose_enabled: bool = matches.contains_id("verbose") && matches.get_flag("verbose");
+    // parse URL from arguments
+    let url: &String = matches.get_one::<String>("url").expect("URL not specified");
+    // parse request data
+    let data: &String = matches
+        .get_one::<String>("data")
+        .expect("Could not get request data");
+    // parse request method
+    let method: &String = matches
+        .get_one::<String>("x-method")
+        .expect("Could not parse HTTP method");
+    // parse request headers
+    let headers: Vec<&str> = matches
+        .get_many::<String>("headers")
+        .unwrap_or_default()
+        .map(|s: &String| s.as_str())
+        .collect();
 }
